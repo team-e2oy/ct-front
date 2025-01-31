3bd81d7ebd1fd3e2aa709c4dc13d10a6d324a4dc
Steady <overclass13@gmail.com>
2025-02-01 00:41:02 +0900

    Oauth2 Login Sample

--- src/sample/components/BeggarLife.tsx
+++ src/sample/components/BeggarLife.tsx
@@ -1,5 +1,6 @@
 import {beggarLifeStore} from "../store/BeggarStore.ts";
 import BeggarEmotion from "./BeggarEmotion.tsx"
+import {useEffect} from "react";
 
 const BeggarLife = () => {
 
@@ -17,6 +18,23 @@ const BeggarLife = () => {
     return <button onClick={beggarLifeStore(state => state.clear)}>전재산 몰수하기</button>
   }
 
+  const GithubLogin = () => {
+    // window.location.href = "https://github.com/login/oauth/authorize?client_id=Ov23liHhRhGErz7N8E4u&redirect_uri=http://localhost:8080/login/oauth2/code/github&scope=user";
+    window.location.href = "http://localhost:8080/oauth2/authorization/github";
+  }
+
+
+  const GithubLoginBtn = () => {
+    return <button onClick={GithubLogin}>깃허브 로그인</button>
+  }
+
+  useEffect(() => {
+      const token = new URLSearchParams(window.location.search).get("token");
+      if (token) {
+          alert('Token : ' + token);
+      }
+  }, []);
+
   return (
     <>
       <h1>거지키우기</h1>
@@ -27,6 +45,7 @@ const BeggarLife = () => {
       <div>
         <BtnGiveMoney />
         <BtnMolsu />
+        <GithubLoginBtn />
       </div>
     </>
   )
